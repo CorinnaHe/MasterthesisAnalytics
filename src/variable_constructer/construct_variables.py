91b68dc042539ef3eefe95dc1332286cc64256a8
Corinna Heinze <hei.cor@web.de>
2026-02-05 17:03:41 +0100

    refactoring

--- src/variable_constructer/construct_variables.py
+++ src/variable_constructer/construct_variables.py
@@ -1,46 +1,78 @@
 import pandas as pd
 
 def _construct_reliance_metrics(trials: pd.DataFrame) -> pd.DataFrame:
-    # AI correctness
-    trials["set_valid"] = (
-        (trials["y_true"].eq("poor") & trials["cp_contains_poor"]) |
-        (trials["y_true"].eq("standard") & trials["cp_contains_standard"]) |
-        (trials["y_true"].eq("good") & trials["cp_contains_good"])
+    trials = trials.copy()
+
+    mask_pp = trials["condition"].isin([1, 2])
+    mask_set = trials["condition"] == 3
+
+    # ai correctness
+    trials["ai_correct"] = pd.NA
+
+    trials.loc[mask_pp, "ai_correct"] = (
+        trials.loc[mask_pp, "point_pred_cal"]
+        .eq(trials.loc[mask_pp, "y_true"])
     )
 
-    trials["pp_correct"] = trials["point_pred_cal"].eq(trials["y_true"])
-
-    # human–AI agreement
-    trials["initial_agree_pp"] = trials["initial_decision"].eq(trials["point_pred_cal"])
-    trials["final_agree_pp"] = trials["final_decision"].eq(trials["point_pred_cal"])
-
-    trials["initial_agree_set"] = (
-        (trials["initial_decision"].eq("good") & trials["cp_contains_good"]) |
-        (trials["initial_decision"].eq("standard") & trials["cp_contains_standard"]) |
-        (trials["initial_decision"].eq("poor") & trials["cp_contains_poor"])
+    trials.loc[mask_set, "ai_correct"] = (
+        (trials.loc[mask_set, "y_true"].eq("poor") &
+         trials.loc[mask_set, "cp_contains_poor"]) |
+        (trials.loc[mask_set, "y_true"].eq("standard") &
+         trials.loc[mask_set, "cp_contains_standard"]) |
+        (trials.loc[mask_set, "y_true"].eq("good") &
+         trials.loc[mask_set, "cp_contains_good"])
     )
 
-    trials["final_agree_set"] = (
-        (trials["final_decision"].eq("good") & trials["cp_contains_good"]) |
-        (trials["final_decision"].eq("standard") & trials["cp_contains_standard"]) |
-        (trials["final_decision"].eq("poor") & trials["cp_contains_poor"])
+    # human-ai agreement
+    trials["initial_agree_ai"] = pd.NA
+    trials["final_agree_ai"] = pd.NA
+
+    trials.loc[mask_pp, "initial_agree_ai"] = (
+        trials.loc[mask_pp, "initial_decision"]
+        .eq(trials.loc[mask_pp, "point_pred_cal"])
+    )
+
+    trials.loc[mask_pp, "final_agree_ai"] = (
+        trials.loc[mask_pp, "final_decision"]
+        .eq(trials.loc[mask_pp, "point_pred_cal"])
+    )
+
+    trials.loc[mask_set, "initial_agree_ai"] = (
+        (trials.loc[mask_set, "initial_decision"].eq("poor") &
+         trials.loc[mask_set, "cp_contains_poor"]) |
+        (trials.loc[mask_set, "initial_decision"].eq("standard") &
+         trials.loc[mask_set, "cp_contains_standard"]) |
+        (trials.loc[mask_set, "initial_decision"].eq("good") &
+         trials.loc[mask_set, "cp_contains_good"])
+    )
+
+    trials.loc[mask_set, "final_agree_ai"] = (
+        (trials.loc[mask_set, "final_decision"].eq("poor") &
+         trials.loc[mask_set, "cp_contains_poor"]) |
+        (trials.loc[mask_set, "final_decision"].eq("standard") &
+         trials.loc[mask_set, "cp_contains_standard"]) |
+        (trials.loc[mask_set, "final_decision"].eq("good") &
+         trials.loc[mask_set, "cp_contains_good"])
     )
 
     # switching behavior
-    trials["switched_to_pp"] = ~trials["initial_agree_pp"] & trials["final_agree_pp"]
-    trials["switched_to_set"] = ~trials["initial_agree_set"] & trials["final_agree_set"]
+    trials["switched_to_ai"] = (
+        ~trials["initial_agree_ai"] & trials["final_agree_ai"]
+    )
 
     # reliance
-    trials["over_reliance_pp"] = trials["final_agree_pp"] & ~trials["pp_correct"]
-    trials["under_reliance_pp"] = ~trials["final_agree_pp"] & trials["pp_correct"]
-    trials["appropriate_reliance_pp"] = ((trials["final_agree_pp"] & trials["pp_correct"]) |
-                                         (~trials["final_agree_pp"] & ~trials["pp_correct"]))
+    trials["over_reliance"] = (
+        trials["final_agree_ai"] & ~trials["ai_correct"]
+    )
 
-    trials["over_reliance_set"] = trials["switched_to_set"] & ~trials["set_valid"]
-    trials["under_reliance_set"] = ~trials["final_agree_set"] & trials["set_valid"]
-    trials["appropriate_reliance_set"] = ((trials["final_agree_set"] & trials["set_valid"]) |
-                                         (~trials["final_agree_set"] & ~trials["set_valid"]))
+    trials["under_reliance"] = (
+        ~trials["final_agree_ai"] & trials["ai_correct"]
+    )
 
+    trials["appropriate_reliance"] = (
+        (trials["final_agree_ai"] & trials["ai_correct"]) |
+        (~trials["final_agree_ai"] & ~trials["ai_correct"])
+    )
     return trials
 
 
